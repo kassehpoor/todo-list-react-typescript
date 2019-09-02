1f39c6203b357b542f0f923cf8b9f98def8e3c1c
kassehpoor <kasehpoor.k@gmail.com>
2019-09-02 15:18:09 +0430

    create other components and router

--- todolist-react-typescript/src/components/signin-component.tsx
+++ todolist-react-typescript/src/components/signin-component.tsx
@@ -4,14 +4,41 @@ type Props = {};
 type State = {};
 
 
-export default class SignInComponent extends React.Component<Props, State>{
-    readonly state: State = {
+export default class SignInComponent extends React.Component {
+    inputUserName: any;
+    inputPassword: any;
 
+    constructor(props: any) {
+        super(props);
+        this.state = {};
+        this.inputUserName = '';
+        this.inputPassword = '';
+    }
+
+    onLogin() {
+
+        // <Authenticate username={this.inputUserName.value} password={this.inputPassword.value} />
+        // goto('todolistComponent');
+        this.setState({});
+    }
+
+    onCancel() {
+
+        // goto('todolistComponent');
+        this.setState({});
     }
 
     render() {
         return (
-            <div></div>
+            <div>
+                <form ref="form" onSubmit={this.onLogin} className="form-inline">
+                    <input autoComplete="off" type="text" ref={(n) => this.inputUserName = n} className="form-control" placeholder="user name..." />
+                    <input autoComplete="off" type="text" ref={(p) => this.inputPassword = p} className="form-control" placeholder="password..." />
+                    <br />
+                    <button type="submit" onClick={this.onLogin} className="btn btn-default">login</button>
+                    <button onClick={this.onCancel} className="btn btn-default">cancel</button>
+                </form>
+            </div>
         )
 
     };
--- todolist-react-typescript/src/components/signup-component.tsx
+++ todolist-react-typescript/src/components/signup-component.tsx
@@ -4,14 +4,47 @@ type Props = {};
 type State = {};
 
 
-export default class SignUpComponent extends React.Component<Props, State>{
-    readonly state: State = {
+export default class SignUpComponent extends React.Component {
+    inputFirstName: any;
+    inputLastName: any;
+    inputUserName: any;
+    inputPassword: any;
 
+    constructor(props: any) {
+        super(props);
+        this.state = {};
+        this.inputFirstName = '';
+        this.inputLastName = '';
+        this.inputPassword = '';
+        this.inputPassword = '';
+    }
+
+    onRegister() {
+
+        // <RegisterUser firstname={this.inputFirstName.value} lastname={this.inputLastName.value} username={this.inputUserName.value} password={this.inputPassword.value} />
+        // goto('todolistComponent');
+        this.setState({});
+    }
+
+    onCancel() {
+
+        // goto('todolistComponent');
+        this.setState({});
     }
 
     render() {
         return (
-            <div></div>
+            <div>
+                <form ref="form" onSubmit={this.onRegister} className="form-inline">
+                    <input autoComplete="off" type="text" ref={(f) => this.inputFirstName = f} className="form-control" placeholder="first name..." />
+                    <input autoComplete="off" type="text" ref={(l) => this.inputLastName = l} className="form-control" placeholder="last name..." />
+                    <input autoComplete="off" type="text" ref={(n) => this.inputUserName = n} className="form-control" placeholder="user name..." />
+                    <input autoComplete="off" type="text" ref={(p) => this.inputPassword = p} className="form-control" placeholder="password..." />
+                    <br />
+                    <button type="submit" onClick={this.onRegister} className="btn btn-default">submit</button>
+                    <button onClick={this.onCancel} className="btn btn-default">cancel</button>
+                </form>
+            </div>
         )
 
     };
