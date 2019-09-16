3b102830b65ff84237c43b395dbfbd5062abbeab
kassehpoor <kasehpoor.k@gmail.com>
2019-09-16 19:02:46 +0430

    Implement userDisplay name by props,state

--- todolist-react-typescript/src/client/components/header-component.tsx
+++ todolist-react-typescript/src/client/components/header-component.tsx
@@ -26,62 +26,33 @@ export default class HeaderComponent extends React.Component<any, any> {
   }
 }
 
-//  Wrong
-// this.setState({
-//   counter: this.state.counter + this.props.increment,
-// });
-
-//  Correct
-// this.setState((state, props) => ({
-//     counter: state.counter + props.increment
-//   }));
-
 /*
-import * as React from 'react';
-import { NavLink } from 'react-router-dom';
-import database from '../database';
-import sm from '../state-manager';
+import * as React from "react";
+import { NavLink } from "react-router-dom";
 
-type Props = {};
-type State = {
-    userDisplayName: string
-};
+export default class HeaderComponent extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+  }
 
-const { Provider, Consumer } = React.createContext({
-    updateUserDisplayName: (name: string) => { },
-});
-
-export default class HeaderComponent extends React.Component<any, State> {
-
-    constructor(props: any) {
-        super(props);
-        let that = this;
-
-        this.updateUserDisplayName('');
-     }
-
-    updateUserDisplayName = (newUserDisplayName: string) => {
-        this.setState({ userDisplayName: newUserDisplayName });
-    };
-
-    render() {
-        const contextValue = { updateUserDisplayName: this.updateUserDisplayName };
-
-        return (
-            <div>
-                <Provider value={contextValue}>
-                    <div>
-                        <NavLink className='btn-Sign' to="/todolist">TodoList</NavLink>
-                        <NavLink className='btn-Sign' to="/signin">Signin</NavLink>
-                        <NavLink className='btn-Sign' to="/signup">Singup</NavLink>
-                    </div>
-                    <div>{this.state.userDisplayName}</div>
-                </Provider>
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div>
+        <span>{"current user is : " + this.props.userDisplayName}</span>
+        <div>
+          <NavLink className="btn-Sign" to="/todolist">
+            TodoList
+          </NavLink>
+          <NavLink className="btn-Sign" to="/signin">
+            Signin
+          </NavLink>
+          <NavLink className="btn-Sign" to="/signup">
+            Singup
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
 }
-
-export const HeaderConsumer = Consumer;
 
 */
